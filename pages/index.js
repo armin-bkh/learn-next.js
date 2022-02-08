bbdd2b861ae1becb78f2ffa028d38edc4c454163
armin-bkh <arminbkh0921@yahoo.com>
2022-02-08 13:18:06 +0330

    Link component navigation

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,12 @@
+import Link from "next/link";
+
 export default function Home() {
-  return <div>Home Page</div>;
+  return (
+    <div>
+      <h1>Home Page</h1>
+      <Link href="/products">
+        <a>products...</a>
+      </Link>
+    </div>
+  );
 }
--- pages/products/index.js
+++ pages/products/index.js
@@ -1,10 +1,29 @@
-const Products = () => {
+import Link from "next/link";
+
+const Products = ({ productId = 100 }) => {
   return (
     <div>
       <ul>
-        <li>Product 1</li>
-        <li>Product 2</li>
-        <li>Product 3</li>
+        <li>
+          <Link href="/products/1">
+            <a>Product 1</a>
+          </Link>
+        </li>
+        <li>
+          <Link href="/products/2">
+            <a>Product 2</a>
+          </Link>
+        </li>
+        <li>
+          <Link href="/products/3">
+            <a>Product 3</a>
+          </Link>
+        </li>
+        <li>
+          <Link href={`/products/${productId}`} replace>
+            <a>Product {productId}</a>
+          </Link>
+        </li>
       </ul>
     </div>
   );
