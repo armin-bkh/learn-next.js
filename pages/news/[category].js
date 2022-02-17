af225817f5d3878e47c60015535b85a61198e42b
armin-bkh <arminbkh0921@yahoo.com>
2022-02-17 15:22:24 +0330

    synamic SSR pre-rendering for filtering articles by their category

---
+++ pages/news/[category].js
@@ -0,0 +1,43 @@
+import Link from "next/link";
+
+export const getServerSideProps = async ({ params: { category } }) => {
+  const response = await fetch(
+    `http://localhost:3001/news?category=${category}`
+  );
+  const data = await response.json();
+  if (data.length === 0) {
+    return {
+      notFound: true,
+    };
+  }
+  return {
+    props: {
+      articles: data,
+      category,
+    },
+  };
+};
+
+const ArticleListByCategory = ({ articles, category }) => {
+  return (
+    <section>
+      <h1>articles list of {category}</h1>
+      <ul>
+        {articles.map((article) => (
+          <li key={article.id}>
+            <Link href={`/news/${article.id}`}>
+              <div>
+                <h2>
+                  {article.id} {article.title}
+                </h2>
+                <p>{article.descirption}</p>
+              </div>
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </section>
+  );
+};
+
+export default ArticleListByCategory;
