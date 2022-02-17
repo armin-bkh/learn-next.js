48e1677528ceafbb315da3442b7cad234f68e625
armin-bkh <arminbkh0921@yahoo.com>
2022-02-17 15:06:15 +0330

    SSR pre-rendering for news page

---
+++ pages/news/index.js
@@ -0,0 +1,33 @@
+import Link from "next/link";
+
+export const getServerSideProps = async () => {
+  const response = await fetch("http://localhost:3001/news");
+  const data = await response.json();
+
+  return {
+    props: {
+      articles: data,
+    },
+  };
+};
+
+const NewsList = ({ articles }) => {
+  return (
+    <section>
+      <h1>List of News articles</h1>
+      <ul>
+        {articles.map((article) => (
+          <li key={article.id}>
+            <Link href={`/news/${article.id}`} passHref>
+              <h2>
+                {article.id} {article.title} {article.category}
+              </h2>
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </section>
+  );
+};
+
+export default NewsList;
