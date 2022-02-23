c69b54899c9341de31f9fc0872fabf661357114a
arminbkh0921@yahoo.com <arminbkh0921@yahoo.com>
2022-02-23 19:57:41 +0330

    create Layout component for wrapping around pages

---
+++ pages/about.js
@@ -0,0 +1,9 @@
+const About = () => {
+    return ( 
+        <main className="main">
+            about page
+        </main>
+     );
+}
+ 
+export default About;
--- pages/index.js
+++ pages/index.js
@@ -4,15 +4,21 @@ export default function Home() {
   return (
     <div>
       pre-rendering next.js
-      <Link href="/users">
-        <a>users</a>
-      </Link>
-      <Link href="/posts">
-        <a>posts</a>
-      </Link>
-      <Link href="/products">
-        <a>products</a>
-      </Link>
+      <h2>
+        <Link href="/users">
+          <a>users</a>
+        </Link>
+      </h2>
+      <h2>
+        <Link href="/posts">
+          <a>posts</a>
+        </Link>
+      </h2>
+      <h2>
+        <Link href="/products">
+          <a>products</a>
+        </Link>
+      </h2>
     </div>
   );
 }
