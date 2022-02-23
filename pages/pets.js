377b20b94a9c2d8c9460836e1a0e39730702a42d
arminbkh0921@yahoo.com <arminbkh0921@yahoo.com>
2022-02-23 21:55:35 +0330

    comment my codes in pets page

--- pages/pets.js
+++ pages/pets.js
@@ -6,10 +6,10 @@ const pets = ['1', '2', '3', '4', '5'];
 const PetsPage = () => {
     return ( 
         <main>
-            <Image src={pet} alt="pet 5" width={500} height={500} placeholder="blur" property="" />
+            {/* <Image src={pet} alt="pet 5" width={500} height={500} placeholder="blur" priority />
             {pets.map(pet => (
                 <Image key={pet} src={`/images/${pet}.jpg`} alt="pets gallery" height={500} width={500}/>
-            ))}
+            ))} */}
         </main>
      );
 }
