b38eaeb5fc70d33e3bd4d809310ca5111ab2f3c6
arminbkh0921@yahoo.com <arminbkh0921@yahoo.com>
2022-02-24 18:55:30 +0330

    use environment variables in next

--- pages/about.js
+++ pages/about.js
@@ -1,12 +1,24 @@
 import Head from "next/head";
 
-const About = () => {
+export const getStaticProps = async () => {
+  const user = process.env.DB_USER;
+  const password = process.env.DB_PASSWORD;
+  console.log(user, password);
+  return {
+    props: {
+      title: "this is about",
+      user
+    }
+  }
+}
+
+const About = ({title, user}) => {
   return (
     <>
       <Head>
         <meta name="description" content="about us page" />
       </Head>
-      <main className="main">about page</main>
+      <main className="main">{title} page {process.env.NEXT_PUBLIC_PASSWORD}</main>
     </>
   );
 };
